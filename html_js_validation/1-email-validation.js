39fd38406e35b7b4d2224e91110990694d35a799
Nancyhun <nancyoforiaddo17@gmail.com>
2024-02-19 13:06:26 -0800

    form commit

---
+++ html_js_validation/1-email-validation.js
@@ -0,0 +1,21 @@
+// function to validate email
+const form = document.getElementById('emailForm');
+const email = document.getElementById('email');
+const error = document.getElementById('error');
+const emailregex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+function validateEmail() {
+    if (!emailregex.test(email.value.trim())) {
+        error.textContent = "Please enter a valid email address.";
+        return false;
+    } else {
+        error.textContent = "";
+        return true;
+    }
+};
+
+form.addEventListener('submit', function(e) {
+    if (!validateEmail()) {
+        e.preventDefault();
+    }
+});
